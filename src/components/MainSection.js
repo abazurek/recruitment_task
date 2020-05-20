1f65ffd3ac445a449ba815f2fca9d30e0bbb0009
abazurek <skala2294@gmail.com>
2020-05-20 23:26:52 +0200

    good works MainSection on big screen, doesn't work on 1024

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -109,8 +109,8 @@ function MainSection() {
                     </div>
                     <ul>
                         {imgTable.map((item) => (
-                            <div>
-                                <li className='single-box-icon' key={imgTable.indexOf(item)}>
+                            <div key={imgTable.indexOf(item)}>
+                                <li className='single-box-icon'>
                                     <NavLink className='naw-link' to={`/${item.string}`}>
                                         <div className='img-box'>
                                             <img src={item.img} alt={item.string}/>
